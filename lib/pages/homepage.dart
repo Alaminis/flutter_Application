419385b23123d70ac7f180cf4fbb21ded544066e
Md Alamin Hossain(tushar) <mamuntokpi2019@gmail.com>
2021-10-11 19:09:50 -0700

    this is first commit

---
+++ lib/pages/homepage.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "MyApp",
+          style: TextStyle(fontSize: 24, color: Colors.red),
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Container(
+          width: 300,
+          height: 200,
+          color: Colors.yellow,
+          child: const Center(
+              child: Text(
+            "This is my fist Application",
+            style: TextStyle(
+                fontSize: 23,
+                color: Colors.red,
+                fontWeight: FontWeight.bold,
+                backgroundColor: Colors.blue),
+          )),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
